84dca3c2334378c8c1806c58f0c69c45c320cd51
luisfgarav <luisfgaravil@gmail.com>
2021-06-17 12:27:42 -0500

    Barra de busqueda agregada

--- app/src/main/java/com/example/thevault/Feed.java
+++ app/src/main/java/com/example/thevault/Feed.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,55 +22,116 @@ import java.util.ArrayList;
 
 public class Feed extends AppCompatActivity {
 
+    private EditText etBusqueda;
+    private ImageButton btnBusqueda;
+    private String nombrePelicula;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        etBusqueda=(EditText)findViewById(R.id.etBusqueda);
+        btnBusqueda=(ImageButton)findViewById(R.id.btnBusqueda);
+
         final ListView lista;
         final Adapter adaptador=new Adapter();
         lista=(ListView)findViewById(R.id.lista);
-        AsyncHttpClient cliente = new AsyncHttpClient();
-        cliente.get(BEConection.URL + "feed.php" ,
-                new AsyncHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
-                        if(statusCode == 200){
-                            try{
-                                String x = new String(responseBody);
-                                if( !x.equals("0")){
-                                    JSONArray peliculas = new JSONArray(new String(responseBody));
-                                    ArrayList<Card> resultado = new ArrayList<Card>();
-                                    for (int i=0;i<peliculas.length();i++){
-                                        Card carta=new Card();
-                                        carta.id=peliculas.getJSONObject(i).getInt("id");
-                                        carta.nombre=peliculas.getJSONObject(i).getString("nombre");
-                                        carta.imagen=peliculas.getJSONObject(i).getString("imagen");
-                                        carta.duracion=peliculas.getJSONObject(i).getString("duracion");
-                                        carta.año=peliculas.getJSONObject(i).getString("fecha_estreno");
-                                        carta.premios=peliculas.getJSONObject(i).getString("premios");
-                                        carta.comentarios=peliculas.getJSONObject(i).getString("comentarios");
-                                        resultado.add(carta);
+
+        if(getIntent().hasExtra("peliculaNombre")) {
+            nombrePelicula = getIntent().getStringExtra("peliculaNombre");
+            AsyncHttpClient cliente = new AsyncHttpClient();
+            cliente.get(BEConection.URL + "buscar_pelicula.php?nombre="+nombrePelicula ,
+                    new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                            if(statusCode == 200){
+                                try{
+                                    String x = new String(responseBody);
+                                    if( !x.equals("0")){
+                                        JSONArray peliculas = new JSONArray(new String(responseBody));
+                                        ArrayList<Card> resultado = new ArrayList<Card>();
+                                        for (int i=0;i<peliculas.length();i++){
+                                            Card carta=new Card();
+                                            carta.id=peliculas.getJSONObject(i).getInt("id");
+                                            carta.nombre=peliculas.getJSONObject(i).getString("nombre");
+                                            carta.imagen=peliculas.getJSONObject(i).getString("imagen");
+                                            carta.duracion=peliculas.getJSONObject(i).getString("duracion");
+                                            carta.año=peliculas.getJSONObject(i).getString("fecha_estreno");
+                                            carta.premios=peliculas.getJSONObject(i).getString("premios");
+                                            carta.comentarios=peliculas.getJSONObject(i).getString("comentarios");
+                                            resultado.add(carta);
+                                        }
+                                        adaptador.arreglo=resultado;
+                                        adaptador.context=Feed.this;
+                                        lista.setAdapter(adaptador);
+                                    } else{
+                                        Toast.makeText(Feed.this, "Pelicula no encontrada.", Toast.LENGTH_SHORT).show();
                                     }
-                                    adaptador.arreglo=resultado;
-                                    adaptador.context=Feed.this;
-                                    lista.setAdapter(adaptador);
-                                } else{
-                                    Toast.makeText(Feed.this, "Pelicula no encontrada.", Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e){
+                                    Toast.makeText(Feed.this, "Error al obtener información.", Toast.LENGTH_SHORT).show();
                                 }
-                            } catch (JSONException e){
-                                Toast.makeText(Feed.this, "Error al obtener información.", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(Feed.this, "Sin resultados en busqueda.", Toast.LENGTH_SHORT).show();
                             }
                         }
-                        else{
-                            Toast.makeText(Feed.this, "Sin resultados en busqueda.", Toast.LENGTH_SHORT).show();
+
+                        @Override
+                        public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+
                         }
-                    }
+                    });
+        } else {
+            AsyncHttpClient cliente = new AsyncHttpClient();
+            cliente.get(BEConection.URL + "feed.php" ,
+                    new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                            if(statusCode == 200){
+                                try{
+                                    String x = new String(responseBody);
+                                    if( !x.equals("0")){
+                                        JSONArray peliculas = new JSONArray(new String(responseBody));
+                                        ArrayList<Card> resultado = new ArrayList<Card>();
+                                        for (int i=0;i<peliculas.length();i++){
+                                            Card carta=new Card();
+                                            carta.id=peliculas.getJSONObject(i).getInt("id");
+                                            carta.nombre=peliculas.getJSONObject(i).getString("nombre");
+                                            carta.imagen=peliculas.getJSONObject(i).getString("imagen");
+                                            carta.duracion=peliculas.getJSONObject(i).getString("duracion");
+                                            carta.año=peliculas.getJSONObject(i).getString("fecha_estreno");
+                                            carta.premios=peliculas.getJSONObject(i).getString("premios");
+                                            carta.comentarios=peliculas.getJSONObject(i).getString("comentarios");
+                                            resultado.add(carta);
+                                        }
+                                        adaptador.arreglo=resultado;
+                                        adaptador.context=Feed.this;
+                                        lista.setAdapter(adaptador);
+                                    } else{
+                                        Toast.makeText(Feed.this, "Pelicula no encontrada.", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e){
+                                    Toast.makeText(Feed.this, "Error al obtener información.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(Feed.this, "Sin resultados en busqueda.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
 
-                    @Override
-                    public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+                        @Override
+                        public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
 
-                    }
-                });
+                        }
+                    });
+        }
+    }
+
+    public void buscarPelicula(View view){
+        Intent feed = new Intent(this,Feed.class);
+        feed.putExtra("peliculaNombre",etBusqueda.getText().toString());
+        startActivity(feed);
     }
 
     public boolean onCreateOptionsMenu(android.view.Menu menu){
