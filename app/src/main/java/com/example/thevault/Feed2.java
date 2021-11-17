1d9e799f4cedf6330d93ff77eaf330aa4f12c295
Jesús Martínez Cortez <c.f.cubers@gmail.com>
2021-11-17 16:38:27 -0600

    Feed 2

---
+++ app/src/main/java/com/example/thevault/Feed2.java
@@ -0,0 +1,82 @@
+package com.example.thevault;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.thevault.ui.main.SectionsPagerAdapter;
+import com.example.thevault.databinding.ActivityFeed2Binding;
+
+public class Feed2 extends AppCompatActivity {
+
+    private ActivityFeed2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityFeed2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+
+    }
+
+    public boolean onCreateOptionsMenu(android.view.Menu menu){
+        SharedPreferences preferencias = getSharedPreferences("user.dat", MODE_PRIVATE);
+        if(preferencias.getBoolean("admin", false)){
+            getMenuInflater().inflate(R.menu.menu_admin, menu);
+        }else{
+            getMenuInflater().inflate(R.menu.menu_principal, menu);
+        }
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case R.id.perfil:
+                Intent perfil = new Intent(this,Perfil.class);
+                startActivity(perfil);
+                break;
+            case R.id.cerrarSesion:
+                cerrarSesion();
+                break;
+            case R.id.editar_funciones:
+                Intent editar = new Intent(this,Perfil.class);
+                startActivity(editar);
+                break;
+            case R.id.leer_boletos:
+                Intent leer = new Intent(this,LectorQR.class);
+                startActivity(leer);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cerrarSesion(){
+        SharedPreferences preferencias = getSharedPreferences("user.dat", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.clear();
+        editor.apply();
+        Intent logout = new Intent(this,Login.class);
+        logout.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(logout);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/thevault/ui/main/PlaceholderFragment.java
@@ -0,0 +1,80 @@
+package com.example.thevault.ui.main;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.thevault.Cartelera;
+import com.example.thevault.Feed;
+import com.example.thevault.Login;
+import com.example.thevault.Perfil;
+import com.example.thevault.R;
+import com.example.thevault.databinding.FragmentFeed2Binding;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+    private FragmentFeed2Binding binding;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentFeed2Binding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.sectionLabel;
+        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
