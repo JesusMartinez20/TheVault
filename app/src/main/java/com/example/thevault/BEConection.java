c13929eb33a1d1071e2e923d5af6f597090eaf6e
Luis Felipe García Avila <luisfgaravil@gmail.com>
2021-11-21 01:21:11 -0600

    Regreso de URL

--- app/src/main/java/com/example/thevault/BEConection.java
+++ app/src/main/java/com/example/thevault/BEConection.java
@@ -2,6 +2,5 @@ package com.example.thevault;
 
 public interface BEConection {
     //String URL="http://192.168.1.65:80/thevault-be/";
-    //String URL="http://192.168.100.191/thevault-be/";
-    String URL="http://192.168.2.7/thevault-be/";
+    String URL="http://192.168.100.191/thevault-be/";
 }
